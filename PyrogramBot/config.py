63fb9f21e0c888a6a7b25ce573b838d66ba741f5
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-07 10:55:43 +0530

    Forgotten the config.py

---
+++ PyrogramBot/config.py
@@ -0,0 +1,4 @@
+BOT_TOKEN = "16383:BSNSJS"
+API_ID = 69
+API_HASH = "HSJSN7288KSNEJD"
+#MONGO_DB_URI = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
