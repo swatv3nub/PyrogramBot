701ea5857d0c63d804b4899be1fcefc7001ab877
sᴡᴏɴɪᴛ <swatv3nub@gmail.com>
2021-04-11 08:57:53 +0530

    Update __init__.py

--- PyrogramBot/__init__.py
+++ PyrogramBot/__init__.py
@@ -10,6 +10,8 @@ from .config import API_ID, API_HASH, BOT_TOKEN
 
 # from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
+LOAD_MODULES = []
+NOLOAD_MODULES = []
 StartTime = time.time()
 logging.basicConfig(level=logging.INFO)
 
